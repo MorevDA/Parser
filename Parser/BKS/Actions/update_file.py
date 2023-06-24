5c4e6ec23eefb1e2351ff812bfa5deabd415cb16
MorevDA <morevda1983@gmail.com>
2023-06-24 21:43:27 +0300

    Удалены импорты неиспользуемых модулей. Скорректирована функция get_update_dict. Введена функция read_json_file для чтения json файлов.

--- Parser/BKS/Actions/update_file.py
+++ Parser/BKS/Actions/update_file.py
@@ -1,36 +1,36 @@
 import json
 from datetime import date
-from pprint import pprint
-
-today = date.today()
-file_name = 'my_stocks_information'
 
 
-def get_full_stocks_list(f_name: str) -> dict:
+file_name = 'my_stock_information'
+name_file_by_date = f'./My_stocks/my_action_{date.today()}'
+
+
+def read_json_file(f_name: str) -> dict:
     with open(f'{f_name}.json', 'r', encoding='utf-8') as f:
-        start_data = json.load(f)
-    return start_data
+        data = json.load(f)
+    return data
 
 
-def get_information_by_date(date: str, result):
-    with open(f'./My_stocks/my_action_{date}.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        for j in data:
-            if j['title'] not in result:
-                result[j['title']] = {f"price at {j['date']}": j['price']}
-            else:
-                result[j['title']].update({f"price at {j['date']}": j['price']})
-    return result
+def get_update_dict(last_day_course: dict, result_dict: dict) -> dict:
+    for j in last_day_course:
+        if j['title'] not in result_dict:
+            result_dict[j['title']] = {f"price at {j['date']}": j['price']}
+        else:
+            result_dict[j['title']].update({f"price at {j['date']}": j['price']})
+    return result_dict
 
 
-def get_actualisation_fail(x):
-    with open('my_stock_information.json', 'w', encoding='utf-8') as file:
-        json.dump(x, file, indent=4, ensure_ascii=False)
+def get_actualisation_fail(f_name: str, inp_dict: dict) -> None:
+    with open(f'{f_name}.json', 'w', encoding='utf-8') as file:
+        json.dump(inp_dict, file, indent=4, ensure_ascii=False)
     print('file_update')
 
 
-def main():
-    get_actualisation_fail(get_information_by_date(today, get_full_stocks_list()))
-
 if __name__ == '__main__':
-    main()
+    my_stocks_dict_all_date = read_json_file(file_name)
+    my_stocks_date_today = read_json_file(name_file_by_date)
+    result = get_update_dict(my_stocks_date_today, my_stocks_dict_all_date)
+    get_actualisation_fail(file_name, result)
+
+
