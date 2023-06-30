2f6db75afad1b7645452df03d48abde8a3bdcf1d
MorevDA <morevda1983@gmail.com>
2023-06-30 22:55:24 +0300

    В функции get_analog_parts и get_original_parts  добавлен метод для получения ориентировочного времени доставки (в днях).

--- Parser/Auto_market/VA/pars_va_json.py
+++ Parser/Auto_market/VA/pars_va_json.py
@@ -12,15 +12,17 @@ def get_original_parts(parts):
     originals = {}
     original_parts = parts.get('data').get('rows').get('request')
     for j, i in enumerate(original_parts):
-        originals[j] = {'delivery_date': i.get('termDate'), 'price': i.get('price'), 'amount': i.get('amount'),
-                        'seller': i.get("destination"), 'seller_rank': i.get('rating')}
+        originals[j] = {'estimated_delivery_days': i.get('termMax'), ' estimated_delivery_date': i.get('termDate'),
+                        'price': i.get('price'), 'amount': i.get('amount'), 'seller': i.get("destination"),
+                        'seller_rank': i.get('rating')}
     pprint(sorted(originals.items(), key=lambda x: x[1]['price']))
 
 
-def get_analog(parts):
+def get_analog_parts(parts):
     analog = {}
     analog_parts = parts.get('data').get('rows').get('nonOriginalAnalog')
     for part in analog_parts:
+        delivery_days: part.get('termMax')
         article = part.get('article')
         id_part = part.get('id')
         brand = part.get('brand')
@@ -28,9 +30,9 @@ def get_analog(parts):
         price = part.get('price')
         amount = part.get('amount')
         date_delivery = part.get('termDate')
-        destination = part.get('destination');
-        offers = {'id': id_part, 'delivery_date': date_delivery, 'amount': amount, 'price': price,
-                  'vendor': destination}
+        destination = part.get("destination");
+        offers = {'id': id_part, 'estimated_delivery_days': delivery_days,  'estimated_delivery_date': date_delivery,
+                  'amount': amount, 'price': price, 'vendor': destination}
         if article not in analog:
             analog[article] = {'brand_name': brand, 'part_name': name, 'offers_list': [offers]}
         else:
@@ -40,9 +42,6 @@ def get_analog(parts):
 
 if __name__ == '__main__':
     x = get_json()
-    pprint(get_analog(x))
-
-
-
+    pprint(get_original_parts(x))
 
 # dict_keys(['request', 'nonOriginalAnalog'])
