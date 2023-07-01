fa46500918e86db50f174b7c398c59e039aea631
MorevDA <morevda1983@gmail.com>
2023-07-01 22:34:04 +0300

    Добавлены описания функций. Скорректированы параметры итогового слованя с аналогами з/ч.  (функция get_analog_parts исключен параметр id_part)

--- Parser/Auto_market/VA/pars_va_json.py
+++ Parser/Auto_market/VA/pars_va_json.py
@@ -3,35 +3,40 @@ from pprint import pprint
 
 
 def get_json():
-    with open('PHC_R3007.json', 'r', encoding='UTF-8') as data_file:
+    """Функция для чтения json файла"""
+    with open('VALEO_R3007_1.json', 'r', encoding='UTF-8') as data_file:
         all_parts_list = json.load(data_file)
     return all_parts_list
 
 
 def get_original_parts(parts):
+    """Функция для сотавления словаря с оригинальными з/x. На вход функции поступает словарь с данными
+    по оригинальным з/x и аналогам. Функция возвращает словарь требуемого формата только с оригинальными з/ч.
+    Исходящий словарь отсортирован по возрастанию цены"""
     originals = {}
     original_parts = parts.get('data').get('rows').get('request')
     for j, i in enumerate(original_parts):
         originals[j] = {'estimated_delivery_days': i.get('termMax'), ' estimated_delivery_date': i.get('termDate'),
                         'price': i.get('price'), 'amount': i.get('amount'), 'seller': i.get("destination"),
                         'seller_rank': i.get('rating')}
-    pprint(sorted(originals.items(), key=lambda x: x[1]['price']))
+    return sorted(originals.items(), key=lambda x: x[1]['price'])
 
 
 def get_analog_parts(parts):
+    """Функция для сотавления словаря с аналогами запасных частей. На вход функции поступает словарь с данными
+        по оригинальным з/ч и аналогам. Функция возвращает словарь требуемого формата только с аналогами."""
     analog = {}
     analog_parts = parts.get('data').get('rows').get('nonOriginalAnalog')
     for part in analog_parts:
-        delivery_days: part.get('termMax')
+        delivery_days = part.get('termMax')
         article = part.get('article')
-        id_part = part.get('id')
         brand = part.get('brand')
         name = part.get('name')
         price = part.get('price')
         amount = part.get('amount')
         date_delivery = part.get('termDate')
         destination = part.get("destination");
-        offers = {'id': id_part, 'estimated_delivery_days': delivery_days,  'estimated_delivery_date': date_delivery,
+        offers = {'estimated_delivery_days': delivery_days,  'estimated_delivery_date': date_delivery,
                   'amount': amount, 'price': price, 'vendor': destination}
         if article not in analog:
             analog[article] = {'brand_name': brand, 'part_name': name, 'offers_list': [offers]}
@@ -43,5 +48,6 @@ def get_analog_parts(parts):
 if __name__ == '__main__':
     x = get_json()
     pprint(get_original_parts(x))
+    pprint(get_analog_parts(x))
 
-# dict_keys(['request', 'nonOriginalAnalog'])
+
