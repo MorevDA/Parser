c4081e1e57c925bf38ce6ad5166185fe0769679c
MorevDA <morevda1983@gmail.com>
2023-07-12 00:20:44 +0300

    Файл для хранения кортежа с перчнем отслеживаемых акций. В дальнейшем планируется добавить функционал по получению актуального перечня акций из личного кабинета БКС.

---
+++ Parser/BKS/Actions/My_equitys.py
@@ -0,0 +1,2 @@
+my_equitys: tuple = ('VKCO', 'ALRS', 'BSPB', 'VTBR', 'CBOM', 'MAGN', 'MTSS', 'MTLR', 'MOEX', 'NLMK', 'NVTK',
+              'OGKB', 'KZOSP', 'POLY', 'RUAL', 'RTKM', 'HYDR', 'SBER', 'CHMF', 'LSRG', 'KZOS', 'EELT')
