fd14d5b05d7e03b398d53be12b519b30dbd1f717
MorevDA <morevda1983@gmail.com>
2023-08-18 22:52:37 +0300

    Init

---
+++ Parser/Auto_market/Emex/emex.py
@@ -0,0 +1,74 @@
+import requests
+from pprint import pprint
+
+from pars_emex import get_analog_or_replacement_parts, get_original_parts
+
+part_number = input('Введите номер детали для поиска по базе_:')
+base_search_url = 'https://emex.ru/api/search/search2'
+alter_search_url = 'https://emex.ru/api/search/search'
+
+headers_search = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Access-Control-Allow-Origin': 'https://emex.ru',
+    'Referer': 'https://emex.ru/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,'
+                  ' like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'cache-control': 'no-cache',
+    'expires': '0',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+}
+
+params_search = {
+    'isHeaderSearch': 'false',
+    'showAll': 'true',
+    'searchString': part_number,
+    'locationId': 23438
+}
+
+alter_params_search = {
+    'detailNum': '',
+    'make': '',
+    'locationId': '23438',
+    'showAll': 'true'
+}
+
+
+def get_search_result(address: str, headers: dict, params: dict) -> dict:
+    """Функция для отправки запроса к API.
+    На вход функции подается url-адрес, хэдер и параметры для запроса. Функция возвращает словарь с результатами
+    поиска для дальнейшей обработки"""
+    ses = requests.Session()
+    search_result = ses.get(url=address,
+                            params=params, headers=headers).json()
+    return search_result['searchResult']
+
+
+result = get_search_result(base_search_url, headers_search, params_search)
+
+match result:
+    case {'noResults': True}:
+        print('По данному номеру детали нет предложений')
+    case {'originals': original, 'analogs': analog, 'replacements': replacement}:
+        pprint(get_original_parts(original))
+        pprint(get_analog_or_replacement_parts(replacement))
+        pprint(get_analog_or_replacement_parts(analog))
+    case {'originals': original, 'analogs': analog}:
+        pprint(get_original_parts(original))
+        pprint(get_analog_or_replacement_parts(analog))
+    case _:
+        part_vars = [{'make': i['make'], 'number': i['num']} for i in result['makes']['list']]
+        alter_params_search['make'], alter_params_search['detailNum'] = part_vars[0].values()
+        alter_result = requests.get(url=alter_search_url, headers=headers_search, params=alter_params_search).json()
+        match alter_result['searchResult']:
+            case {'originals': original, 'analogs': analog, 'replacements': replacement}:
+                pprint(get_original_parts(original))
+                pprint(get_analog_or_replacement_parts(replacement))
+                pprint(get_analog_or_replacement_parts(analog))
+            case {'originals': original, 'analogs': analog}:
+                pprint(get_original_parts(original))
+                pprint(get_analog_or_replacement_parts(analog))
---
+++ Parser/Auto_market/Emex/pars_emex.py
@@ -0,0 +1,48 @@
+import json
+from pprint import pprint
+
+
+def get_analog_or_replacement_parts(parts_dict: dict, sorting_option='Цена, руб.') -> dict:
+    """ Функция для получения информации по аналогам запчасти, как того же производителя, так и других
+    производителей(формат данных в обоих случаях идентичен).
+    Функция принимает на вход словарь с 'сырыми' данными полученными в результате скрейпинга сайта.
+    Функция возвращает словарь с данными приведенными в удобный для пользователя формат.
+    По умолчанию выполняется сортировка по цене, опционально возможно менять параметр сортировки при
+    вызове функции."""
+    analog = {}
+    for i in parts_dict:
+        offers = []
+        for j in i.get('offers'):
+            delivery: int = j['delivery']['value']
+            price: float = j['displayPrice']['value']
+            amount: int = j['lotQuantity']
+            part_info: dict = {'Срок доставки, дней': delivery, 'Цена, руб.': price, 'Количество': amount}
+            offers.append(part_info)
+        offers.sort(key=lambda x: x[sorting_option])
+        analog[f"{i.get('make')} {i.get('detailNum')}"] = offers[: 5]
+    return analog
+
+
+def get_original_parts(parts_list: dict, sorting_option='Цена, руб.') -> list:
+    """ Функция для получения информации по запчасти в соответствии с запрошенным номером.
+        Функция принимает на вход словарь с 'сырыми' данными полученными в результате скрейпинга сайта.
+        Функция возвращает словарь с данными приведенными в удобный для пользователя формат.
+        По умолчанию выполняется сортировка по цене, опционально возможно менять параметр сортировки при
+        вызове функции."""
+    original = []
+    for part in parts_list[0]['offers']:
+        delivery = part['delivery']['value']
+        price = part['displayPrice']['value']
+        amount = part['quantity']
+        part_info = {'Срок доставки, дней': delivery, 'Цена, руб.': price, 'Доступно для заказа': amount}
+        original.append(part_info)
+    original.sort(key=lambda x: x[sorting_option])
+    return original[:5]
+
+
+if __name__ == "__main__":
+    with open('emex_4475005002_1', 'r', encoding='utf-8') as file:
+        search_result = json.load(file)['searchResult']
+    print(search_result.keys())
+    sorting_options = {0: 'Срок доставки, дней', 1: 'Цена, руб.'}
+    pprint(get_analog_or_replacement_parts(search_result['analogs']))
